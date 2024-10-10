042095f21d2a7414845ad6f09d0a8770eca8cbc4
zy <zhuyongdev@163.com>
2024-10-10 11:35:07 +0800

    统计脚本

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,21 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+
     integrations: [starlight({
+        head: [
+            // 示例：添加 Fathom 分析脚本标签。
+            {
+                tag: 'script',
+                content:`
+                    (function(c,l,a,r,i,t,y){
+                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                    })(window, document, "clarity", "script", "l64sbvxcpc");
+                `
+            },
+        ],
         title: 'IWHY.DEV',
 		customCss: [
 			// 你的 Tailwind 基础样式的相对路径
